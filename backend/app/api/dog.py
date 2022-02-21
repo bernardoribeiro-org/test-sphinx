51be7a9b1e811a51328548efa0d79797edd36875
paulogil2010 <paulogil2010@gmail.com>
2022-02-21 20:57:05 -0300

    test html

--- backend/app/api/dog.py
+++ backend/app/api/dog.py
@@ -19,7 +19,15 @@ DOGS = [
 
 @dog_api.route("/")
 class DogList(Resource):
-    """defines Dog list endpoint"""
+    """_summary_
+
+    Args:
+        Resource (class): flask-restx Resource class
+
+    Returns:
+        response (class): Http response with list of dogs
+    """
+
     @dog_api.doc("list_dogs")
     @dog_api.marshal_list_with(dog)
     def get(self):
@@ -31,11 +39,27 @@ class DogList(Resource):
 @dog_api.param("id", "The dog identifier")
 @dog_api.response(404, "Dog not found")
 class Dog(Resource):
-    """defines Dog GET by id endpoint"""
+    """_summary_
+
+    Args:
+        Resource (class): flask-restx Resource class
+
+    Returns:
+        response (class): Http response with list of dogs
+    """
+
     @dog_api.doc("get_dog")
     @dog_api.marshal_with(dog)
     def get(self, id):
-        """Fetch a dog given its identifier"""
+        """Fetch a dog given its identifier
+
+        Args:
+            id (int): Dog unique identifier
+
+        Returns:
+            response (class): Http response with filtered of dog
+        """
+
         for dog in DOGS:
             if dog["id"] == id:
                 return dog
